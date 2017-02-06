c3e3533628c142f5212c474d21be0b4fe1421ace
cristian.schuszter <cschuszter@metabiota.com>
2017-02-06 13:46:19 +0200

    Added final implementation + polling

--- ui/app.js
+++ ui/app.js
@@ -1,13 +1,71 @@
+Vue.use(VueResource);
+
+
+Vue.component('tweet-result', {
+    props: ['tweet'],
+    template: '<tr><td>{{tweet.id}}</td><td>{{tweet.text}}</td>' +
+    '<td>{{tweet.created_at}}</td><td>{{tweet.username}}</td><td>{{tweet.latitude}}</td>' +
+    '<td>{{tweet.longitude}}</td></tr>'
+});
+
+
+Vue.component('tweet-place-result', {
+    props: ['tweet'],
+    template: '<tr><td>{{tweet.id}}</td><td>{{tweet.country}}</td>' +
+    '<td>{{tweet.fullname}}</td><td>{{tweet.count}}</td>'
+});
+
+Vue.component('tweet-hashtag-result', {
+    props: ['tweet'],
+    template: '<tr><td>{{tweet.id}}</td><td>#{{tweet.text}}</td>' +
+    '<td>{{tweet.count}}</td>'
+});
+
 var vm = new Vue({
     el: "#app",
     data: {
-        isValid: true,
-        message: 'This is rendered by Vue'
+        tweets: [],
+        tweetplaces: [],
+        tweethashtags: [],
+        pollServer: false
     },
     methods: {
-        hello: () => {
-            console.log("hello, world");
+        getTweetData: () => {
+            vm.$http.get('/api/tweets').then(response => {
+                vm.tweets = response.body;
+                console.log("Loaded tweet data");
+            });
+        },
+        getTweetPlaceData: () => {
+            vm.$http.get('/api/tweets/places').then(response => {
+                vm.tweetplaces = response.body;
+                console.log("Loaded tweet place data");
+            });
+        },
+        getTweethashtagData: () => {
+            vm.$http.get('/api/tweets/hashtags').then(response => {
+                vm.tweethashtags = response.body;
+                console.log("Loaded tweet place data");
+            });
+        },
+        doPoll: () => {
+            if(vm.pollServer) {
+                initTweets();
+                console.log("Initiated server poll");
+            } else {
+                console.log("Disabled server polling mechanism");
+            }
         }
     }
 });
 
+function initTweets() {
+    vm.getTweetData();
+    vm.getTweetPlaceData();
+    vm.getTweethashtagData();
+    if (vm.pollServer) {
+        setTimeout(initTweets, 1500);
+    }
+}
+
+initTweets();
